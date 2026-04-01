7f06836cb2b3e4fcad530be87c083206e64ba8e8
Jaakko Airikkala <jaakkel@gmail.com>
2026-04-01 14:33:35 +0300

    Add whitenoise

--- project/config/settings.py
+++ project/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_k6xf7%^p%*7%)x+q@ymzv5m(9$q!8+45_jz8hz2$urbtn1tat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 LOGIN_URL = "/polls/login/"
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
